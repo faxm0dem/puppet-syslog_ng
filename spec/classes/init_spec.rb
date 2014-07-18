ce4924c8d070f551128d89dadfae80726ab929d9
btibi <ihrwein@gmail.com>
2014-07-18 22:25:47 +0200

    Uncomment test.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 describe 'syslog_ng' do
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('syslog_ng') }
+  context 'With params defined correctly' do
+    let(:params) {{
+      :config_file => '/tmp/puppet-test/syslog-ng.conf',
+      :sbin_path => '/home/tibi/install/syslog-ng',
+      :purge_syslog_ng_conf => true
+    }}
+    # it {
+    #   should contain_file('/tmp/puppet-test/syslog-ng.conf')
+    # }
   end
 end
