6612d22ec2db6408d18d8eb82b816550c581c365
Tibor Benke <ihrwein@gmail.com>
2014-07-23 23:38:18 +0200

    Style fixes in ruby codes.

--- spec/functions/generate_src_dst_spec.rb
+++ spec/functions/generate_src_dst_spec.rb
@@ -13,48 +13,40 @@ describe 'generate_src_dst' do
                         'options' => { 'file' => "/var/log/apache.log",
                                        'follow_freq' => 1}
         }}
-    let(:expected) {
+        let(:expected) {
 'source s_gsoc {
     file(
         file("/var/log/apache.log"),
         follow_freq(1)
     );
-};'
-}
-    it 'Should generate a simple file source' do
-        result = scope.function_generate_src_dst([source, title, params])
-        expect(result).to be_a String
-        expect(result).to eq expected
-    end
- 
-
+};'     }
+        it 'Should generate a simple file source' do
+            result = scope.function_generate_src_dst([source, title, params])
+            expect(result).to be_a String
+            expect(result).to eq expected
+        end
     end
 
-
-
-  context "With options" do
-    let(:title) { 's_gsoc' } 
-    let(:params) {{ 'type' => 'file',
-                    'options' => { 'file' => "/var/log/apache.log",
-                                   'follow_freq' => 1,
-                                   'flags' => ['no_parse','validate_utf8']}
-    }}
-    let(:expected) {
+    context "With options" do
+        let(:title) { 's_gsoc' } 
+        let(:params) {{ 'type' => 'file',
+                        'options' => { 'file' => "/var/log/apache.log",
+                                       'follow_freq' => 1,
+                                       'flags' => ['no_parse','validate_utf8']}
+        }}
+        let(:expected) {
 'destination s_gsoc {
     file(
         file("/var/log/apache.log"),
         flags(no_parse, validate_utf8),
         follow_freq(1)
     );
-};'
+};'     }
 
-    }
-
-    it 'Should fill the options statement' do
-      result = scope.function_generate_src_dst([destination, title, params])
-      expect(result).to be_a String
-      expect(result).to eq expected
+        it 'Should fill the options statement' do
+          result = scope.function_generate_src_dst([destination, title, params])
+          expect(result).to be_a String
+          expect(result).to eq expected
+        end
     end
-  end
-
 end
