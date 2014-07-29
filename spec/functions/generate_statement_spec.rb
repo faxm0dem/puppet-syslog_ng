27c50007ab87aa0b22dcd8b0fdd48b5dc7e4387a
Tibor Benke <ihrwein@gmail.com>
2014-07-29 08:02:23 +0200

    Add generate_statement function.

--- spec/functions/generate_statement_spec.rb
+++ spec/functions/generate_statement_spec.rb
@@ -3,21 +3,18 @@ require 'spec_helper'
 describe 'generate_statement' do
 
     let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
-
-    context "With simple options" do
-        let(:title) {'r_name'}
-        let(:type) {'rewrite'}
-        let(:params) {[
-                'type' => 'subst',
-                'options' => [
-                    "string",
-                    "replacement",
-                    { 'value' => ['field'] },
-                    { 'flags' => []}
-                ]
-            ]
-        }
-        let(:expected) {
+    let(:title) {'r_name'}
+    let(:type) {'rewrite'}
+    let(:param1) {{
+        'type' => 'subst',
+        'options' => [
+            "string",
+            "replacement",
+            { 'value' => ['field'] },
+            { 'flags' => []}
+        ]
+    }}
+    let(:param1_expected) {
 'rewrite r_name {
     subst(
         "string",
@@ -26,8 +23,93 @@ describe 'generate_statement' do
         flags()
     );
 };
-'     }
+'}
+#    it 'Should raise an exception with not Hash or Array param' do
+#        result = scope.function_generate_statement([title, type, param1])
+#        expect{scope.function_generate_statement([title, type, param1])}.to raise_error
+#    end
+
+    context "Array notation with one item can be omitted" do
+        let(:param1) {{
+            'type' => 'subst',
+            'options' => [
+                "string",
+                "replacement",
+                { 'value' => 'field' },
+                { 'flags' => ''}
+            ]
+        }}
+        it 'On parameters of options' do
+            result = scope.function_generate_statement([title, type, [param1]])
+            expect(result).to be_a String
+            expect(result).to eq param1_expected
+        end
+
+        it 'On options' do
+            result = scope.function_generate_statement([title, type, param1])
+            expect(result).to be_a String
+            expect(result).to eq param1_expected
+        end
+ 
+    end
+
+    context "With simple options" do
+        let(:params) {[param1]}
         it 'Should generate rewrite rule' do
+            result = scope.function_generate_statement([title, type, params])
+            expect(result).to be_a String
+            expect(result).to eq param1_expected
+        end
+    end
+
+    context "Array statement" do
+        let(:type) { 'source' }
+        let(:title) { 's_external' }
+        let(:params) {[
+            { 'type' => 'udp',
+              'options' => [
+                {'ip' => ['192.168.42.2']},
+                {'port' => [514]}
+                ]
+            },
+            { 'type' => 'tcp',
+              'options' => [
+                {'ip' => ['192.168.42.2']},
+                {'port' => [514]}
+                ]
+            },
+            {
+              'type' => 'syslog',
+              'options' => [
+                {'flags' => ['no-multi-line', 'no-parse']},
+                {'ip' => ['10.65.0.5']},
+                {'keep-alive' => ['yes']},
+                {'keep_hostname' => ['yes']},
+                {'transport' => ['udp']}
+                ]
+            }
+        ]}
+      let(:expected) {
+'source s_external {
+    udp(
+        ip(192.168.42.2),
+        port(514)
+    );
+    tcp(
+        ip(192.168.42.2),
+        port(514)
+    );
+    syslog(
+        flags(no-multi-line, no-parse),
+        ip(10.65.0.5),
+        keep-alive(yes),
+        keep_hostname(yes),
+        transport(udp)
+    );
+};
+'}
+
+        it 'Should generate source rule' do
             result = scope.function_generate_statement([title, type, params])
             expect(result).to be_a String
             expect(result).to eq expected
