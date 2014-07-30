4d271e62ab1d5202e5f9e8c7ca67b57503778649
Tibor Benke <ihrwein@gmail.com>
2014-07-30 19:44:26 +0200

    Remove unnecesary files after source and destination refactor.

--- lib/puppet/parser/functions/generate_destination.rb
+++
@@ -1,9 +0,0 @@
-module Puppet::Parser::Functions
-  newfunction(:generate_destination, :type => :rvalue) do |args|
-    Puppet::Parser::Functions.function('generate_src_dst')
-    id = args[0]
-    type = args[1]
-    options = args[2]
-    function_generate_src_dst(["destination", id, type, options])
-  end
-end
