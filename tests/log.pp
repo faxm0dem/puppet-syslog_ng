2400127a46cb95c58ccffcb76e31871df74f5435
Tibor Benke <ihrwein@gmail.com>
2014-07-30 22:02:56 +0200

    Add tests and a complett site.pp
    Conflicts:
    	tests/destination.pp
    	tests/log.pp
    	tests/source.pp

--- tests/log.pp
+++ tests/log.pp
@@ -1,21 +1,6 @@
 syslog_ng::log {'l':
     params => [
-        {'source' => 's_gsoc2014'},
-        {'junction' => [
-                {
-                'channel' => [
-                    {'filter' => 'f_json'},
-                    {'parser' => 'p_json'}
-                    ]
-                },
-                {
-                'channel' => [
-                    {'filter' => 'f_not_json'},
-                    {'flags' => 'final'}
-                ]
-                }
-            ]
-        },
-        {'destination' => 'd_gsoc'}
+        {'source' => 's_external'},
+        {'destination' => 'd_udp'}
     ]
 }
