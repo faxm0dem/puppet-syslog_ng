d757459085c11bc05f7c0fbb9df0dbcd5f5f870f
Tibor Benke <ihrwein@gmail.com>
2014-08-01 14:24:33 +0200

    Add syntax_check_before_reloads parameter to syslog_ng class.

--- manifests/source.pp
+++ manifests/source.pp
@@ -1,12 +1,14 @@
 define syslog_ng::source (
 	$params = []
 ) {
+    include syslog_ng::params
+
     $type = 'source'
     $id = $title
     $order = '60'
     
     concat::fragment { $title:
-        target  => $::syslog_ng::params::config_file,
+        target  => "$syslog_ng::tmp_config_file",
         content => generate_statement($id, $type, $params),
         order => $order
     }
