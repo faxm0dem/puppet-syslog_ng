bebc865ad3e3e7ad4c01e044b6f013fff22e138b
btibi <ihrwein@gmail.com>
2014-08-01 22:50:54 +0200

    Add new example for syslog_ng::log.

---
+++ tests/log_02.pp
@@ -0,0 +1,21 @@
+include syslog_ng
+
+syslog_ng::log {'l2':
+    params => [
+        {'source' => 's_gsoc2014'},
+        {'junction' => [
+            {
+            'channel' => [
+                {'filter' => 'f_json'},
+                {'parser' => 'p_json'}
+            ]},
+            {
+            'channel' => [
+                {'filter' => 'f_not_json'},
+                {'flags' => 'final'}
+            ]}
+        ]
+        },
+        {'destination' => 'd_gsoc'}
+    ]
+}
